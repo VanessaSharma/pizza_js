7b44c52ae7cb029b9bb8ec8996d257ccb1af9fc1
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-07-26 08:17:27 -0700

    Add css,js logic, images,and css.

---
+++ js/logic.js
@@ -0,0 +1,26 @@
+// Required logic
+
+	//toppings and size of customer's pizza
+var yourPizza = {
+  toppings = [],
+  size = "";
+}
+
+	//pizza object constructor
+function Pizza(toppings, size) {
+  this.toppings = toppings;
+  this.size = size;
+}
+
+	//prototype method
+Pizza.prototype.toppingsSum = function(topping) {
+  forEach(yourPizza.toppings)
+}
+
+// UI logic
+$(document).ready(function() {
+  $("form#pizza-choices").submit(function(event) {
+
+  });
+});
+
